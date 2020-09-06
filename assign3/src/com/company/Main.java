d1d76679bb4c0d698ac53bfbab11bc5f9ccd5789
Chetan Tankala <ctankala3@gatech.edu>
2020-09-06 17:32:50 -0400

    Assign 3 test on git

---
+++ assign3/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a =2;
+	int b =4;
+	System.out.println("this is a test" + a+b);
+
+	Test_class testclass = new Test_class(8,9);
+        System.out.println(testclass.a);
+    }
+}
---
+++ assign3/src/com/company/Test_class.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Test_class {
+    int a;
+    int b;
+
+    public Test_class(int a, int b) {
+        this.a = a;
+        this.b = b;
+        System.out.println("this is a test constructor");
+    }
+}
